a3b7d196991ce5ce01ea0f1d2ef323d169e8bfb0
caoruipeng <1014408159@qq.com>
2021-08-03 17:01:48 +0800

    44:`:```拟第一次提交，然后撤销
    `

---
+++ login.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/expect
+#设置ssh连接的用户名
+set user $1
+s
+#设置ssh连接的host地址
+set host $2
+
+#设置ssh连接的端口号
+set port $3
+
+#设置ssh连接的登录密码
+set password $4
+
+#设置连接超时时间
+set timeout -1
+
+spawn ssh -o StrictHostKeyChecking=no $user@$host -p $port
+expect "*password:"
+
+#提交密码
+send "$password\r"
+
+#控制权移交
+interact
